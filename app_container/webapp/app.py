b050cc42b685da739abd146f3bf747cbda60b952
gregf <gfelice@cablevision.com>
2014-10-29 10:39:26 -0400

    flask app that can talk to the database container, return the string from the database via http output stream

--- app_container/webapp/app.py
+++ app_container/webapp/app.py
@@ -1,5 +1,5 @@
-
 import os
+import psycopg2
 
 from flask import Flask
 
@@ -8,7 +8,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     provider = str(os.environ.get('PROVIDER', 'world'))
-    return 'Helloooooooooooo Tuesday ' + provider + '!'
+    conn = psycopg2.connect("dbname=docker user=postgres host=db port=5432")
+    # conn = psycopg2.connect("dbname=docker user=postgres host=172.16.107.93 port=49153")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM person;")
+    s = cur.fetchone()
+    cur.close()
+    conn.close()
+    return 'from the database comes the name: ' + s[1] + ' ' + s[0]
 
 @app.route('/health')
 def health():
@@ -18,3 +25,4 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
